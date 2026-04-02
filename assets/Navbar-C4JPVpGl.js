a4d654a6bac15f25259b6544a3dd688ee1b29413
sinchanghosal2 <ssinchangghosal@outlook.com>
2026-04-02 14:18:11 +0530

    Updates

--- assets/Navbar-C4JPVpGl.js
+++ assets/Navbar-C4JPVpGl.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-DeUQhp6h.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-kSHXJEAB.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
